c16ab6b6396179c4371ece9b9d2249fc4dc7b50a
Hungbok <bjminimn@naver.com>
2021-03-19 23:53:41 +0900

    0.0019

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -64,6 +64,146 @@ $(document).ready(function() {
       $panel.fadeIn();
   });
 
+  $("#btn_popup_open3").on("click", function(e) {
+
+      // 팝업 가운데 설정(가로)
+      if ($panelContents.outerWidth() < $(document).width()) {
+          $panelContents.css("margin-left", "-" + $panelContents.outerWidth() / 2 + "px");
+      } else {
+          $panelContents.css("left", "0px");
+      }
+
+      // 팝업 가운데 설정(세로)
+      if ($panelContents.outerHeight() < $(document).height()) {
+          $panelContents.css("margin-top", "-" + $panelContents.outerHeight() / 2 + "px");
+      } else {
+          $panelContents.css("top", "0px");
+      }
+
+      // 레이어 팝업 열기
+      $panel.fadeIn();
+  });
+
+  $("#btn_popup_open4").on("click", function(e) {
+
+      // 팝업 가운데 설정(가로)
+      if ($panelContents.outerWidth() < $(document).width()) {
+          $panelContents.css("margin-left", "-" + $panelContents.outerWidth() / 2 + "px");
+      } else {
+          $panelContents.css("left", "0px");
+      }
+
+      // 팝업 가운데 설정(세로)
+      if ($panelContents.outerHeight() < $(document).height()) {
+          $panelContents.css("margin-top", "-" + $panelContents.outerHeight() / 2 + "px");
+      } else {
+          $panelContents.css("top", "0px");
+      }
+
+      // 레이어 팝업 열기
+      $panel.fadeIn();
+  });
+
+  $("#btn_popup_open5").on("click", function(e) {
+
+      // 팝업 가운데 설정(가로)
+      if ($panelContents.outerWidth() < $(document).width()) {
+          $panelContents.css("margin-left", "-" + $panelContents.outerWidth() / 2 + "px");
+      } else {
+          $panelContents.css("left", "0px");
+      }
+
+      // 팝업 가운데 설정(세로)
+      if ($panelContents.outerHeight() < $(document).height()) {
+          $panelContents.css("margin-top", "-" + $panelContents.outerHeight() / 2 + "px");
+      } else {
+          $panelContents.css("top", "0px");
+      }
+
+      // 레이어 팝업 열기
+      $panel.fadeIn();
+  });
+
+  $("#btn_popup_open6").on("click", function(e) {
+
+      // 팝업 가운데 설정(가로)
+      if ($panelContents.outerWidth() < $(document).width()) {
+          $panelContents.css("margin-left", "-" + $panelContents.outerWidth() / 2 + "px");
+      } else {
+          $panelContents.css("left", "0px");
+      }
+
+      // 팝업 가운데 설정(세로)
+      if ($panelContents.outerHeight() < $(document).height()) {
+          $panelContents.css("margin-top", "-" + $panelContents.outerHeight() / 2 + "px");
+      } else {
+          $panelContents.css("top", "0px");
+      }
+
+      // 레이어 팝업 열기
+      $panel.fadeIn();
+  });
+
+  $("#btn_popup_open7").on("click", function(e) {
+
+      // 팝업 가운데 설정(가로)
+      if ($panelContents.outerWidth() < $(document).width()) {
+          $panelContents.css("margin-left", "-" + $panelContents.outerWidth() / 2 + "px");
+      } else {
+          $panelContents.css("left", "0px");
+      }
+
+      // 팝업 가운데 설정(세로)
+      if ($panelContents.outerHeight() < $(document).height()) {
+          $panelContents.css("margin-top", "-" + $panelContents.outerHeight() / 2 + "px");
+      } else {
+          $panelContents.css("top", "0px");
+      }
+
+      // 레이어 팝업 열기
+      $panel.fadeIn();
+  });
+
+  $("#btn_popup_open8").on("click", function(e) {
+
+      // 팝업 가운데 설정(가로)
+      if ($panelContents.outerWidth() < $(document).width()) {
+          $panelContents.css("margin-left", "-" + $panelContents.outerWidth() / 2 + "px");
+      } else {
+          $panelContents.css("left", "0px");
+      }
+
+      // 팝업 가운데 설정(세로)
+      if ($panelContents.outerHeight() < $(document).height()) {
+          $panelContents.css("margin-top", "-" + $panelContents.outerHeight() / 2 + "px");
+      } else {
+          $panelContents.css("top", "0px");
+      }
+
+      // 레이어 팝업 열기
+      $panel.fadeIn();
+  });
+
+  $("#btn_popup_open9").on("click", function(e) {
+
+      // 팝업 가운데 설정(가로)
+      if ($panelContents.outerWidth() < $(document).width()) {
+          $panelContents.css("margin-left", "-" + $panelContents.outerWidth() / 2 + "px");
+      } else {
+          $panelContents.css("left", "0px");
+      }
+
+      // 팝업 가운데 설정(세로)
+      if ($panelContents.outerHeight() < $(document).height()) {
+          $panelContents.css("margin-top", "-" + $panelContents.outerHeight() / 2 + "px");
+      } else {
+          $panelContents.css("top", "0px");
+      }
+
+      // 레이어 팝업 열기
+      $panel.fadeIn();
+  });
+
   // 팝업 닫기 이벤트 정의
   $("#btn_popup_close").on("click", popupClose);
 
